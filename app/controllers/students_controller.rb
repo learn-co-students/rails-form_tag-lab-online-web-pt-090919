3d0db18d97fb252c7890df9506f638ed9c47bc6d
Sarah Condon <s.j.condon01@gmail.com>
2020-02-03 15:37:55 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,26 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  # it 'creates a record in the database' do
+  #   visit new_student_path
+
+  #   fill_in 'first_name', with: "Sansa"
+  #   fill_in 'last_name', with: "Stark"
+
+  #   click_on "Submit Student"
+
+  #   expect(Student.last.first_name).to eq("Sansa")
+  # end
+
 end
